e422aa30feec8718a1e00b14067cbfa3f6abf495
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-07 21:35:46 +0000

    rec

--- Intellia/src/Pages/UserDashBoard.jsx
+++ Intellia/src/Pages/UserDashBoard.jsx
@@ -1,35 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-const UserDashboard = () => {
-  const [transcript, setTranscript] = useState('');
-  const [listening, setListening] = useState(false);
-  const { finalTranscript, resetTranscript } = useSpeechRecognition();
-
-  useEffect(() => {
-    if (finalTranscript) {
-      setTranscript(finalTranscript);
-      resetTranscript();
-    }
-  }, [finalTranscript, resetTranscript]);
-
-  const toggleListening = () => {
-    if (!listening) {
-      SpeechRecognition.startListening();
-    } else {
-      SpeechRecognition.stopListening();
-    }
-    setListening(!listening);
-  };
-
+function UserDashBoard() {
   return (
-    <div>
-      <button onClick={toggleListening}>
-        {listening ? 'Stop listening' : 'Start listening'}
-      </button>
-      <p>Transcript: {transcript}</p>
-    </div>
-  );
-};
+    <div>UserDashBoard</div>
+  )
+}
 
-export default UserDashboard;
+export default UserDashBoard
