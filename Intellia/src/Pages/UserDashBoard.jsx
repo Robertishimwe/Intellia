baca4cc510b3a21e13ab0ac4a72d0987aa230274
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-07 21:55:05 +0000

    rec

--- Intellia/src/Pages/UserDashBoard.jsx
+++ Intellia/src/Pages/UserDashBoard.jsx
@@ -28,16 +28,44 @@
 // export default UserDashBoard
 
 
-import React from 'react'
-// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+// import React from 'react'
+// // import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-function UserDashBoard() {
-  return (
-    <>
-    <div>UserDashBoard</div>
-    </>
+// function UserDashBoard() {
+//   return (
+//     <>
+//     <div>UserDashBoard</div>
+//     </>
     
-  )
-}
+//   )
+// }
 
-export default UserDashBoard
+// export default UserDashBoard
+
+
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+const UserDashBoard = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default UserDashBoard;
