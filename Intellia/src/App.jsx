0226e8e38aa227e2f2e3a32f88e3c4224ff2a19f
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-07 19:33:43 +0000

    pp

--- Intellia/src/App.jsx
+++ Intellia/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Route, Routes } from 'react-router-dom';
+
 import LoginPage from "./Pages/LoginPage";
 import UserDashBoard from "./Pages/UserDashBoard"
 import "./App.css";
@@ -6,10 +8,10 @@ import "./App.css";
 function App() {
 
   return (
-    <div className="App">
-      <LoginPage />
-      <UserDashBoard/>
-    </div>
+    <Routes>
+			<Route path='/' element={<UserDashBoard/>} />
+      <Route path='/login' element={<LoginPage />} />
+    </Routes>
   );
 }
 
