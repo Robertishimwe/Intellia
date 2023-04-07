9aff172950f2f1c890b1ecd7bacf7b93a20235c3
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-07 21:37:13 +0000

    rec

--- Intellia/src/Pages/UserDashBoard.jsx
+++ Intellia/src/Pages/UserDashBoard.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 function UserDashBoard() {
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
   return (
     <div>UserDashBoard</div>
   )
