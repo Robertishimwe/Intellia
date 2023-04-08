bbe5cd50f4a6b16ac66664105616ae38f6263720
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-08 13:16:57 +0000

    auth part

--- Intellia/src/main.jsx
+++ Intellia/src/main.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'regenerator-runtime/runtime'
-import App from './App'
 import './index.css'
+
+import store from './redux/store';
+import App from './App'
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
