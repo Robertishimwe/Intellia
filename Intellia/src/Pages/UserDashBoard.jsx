92028f2fd9a1497394e05b6a6eacaa4a7fe6e24a
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-08 21:16:31 +0000

    m

--- Intellia/src/Pages/UserDashBoard.jsx
+++ Intellia/src/Pages/UserDashBoard.jsx
@@ -1,11 +1,13 @@
-// import React from 'react'
+// import React, {useState, useEffect} from 'react';
 // import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-// function UserDashBoard() {
+// import api from '../utils/api';
 
+// const UserDashBoard = () => {
 //   const {
 //     transcript,
 //     listening,
+//     finalTranscript,
 //     resetTranscript,
 //     browserSupportsSpeechRecognition
 //   } = useSpeechRecognition();
@@ -14,45 +16,64 @@
 //     return <span>Browser doesn't support speech recognition.</span>;
 //   }
 
-//   return (
+// const [botResponse, setBotResponse] = useState('')
+// const [trigger, setTrigger] = useState(false)
 
-//     <>
+
+// useEffect(() => {
+//   console.log(listening)
+ 
+// if(finalTranscript =="" || finalTranscript == null || finalTranscript == undefined){
+//   console.log("==>",listening)
+//   SpeechRecognition.startListening()
+// }
+
+// }, [listening])
+
+
+// useEffect(() => {
+
+//   if(finalTranscript){
+//     //axios
+//     console.log(finalTranscript)
+//     api.post(`/chat/test`,{"prompt":finalTranscript})
+//         .then((res) => {
+//           console.log(res.data.bot)
+//           resetTranscript()
+         
+//         }).then(()=>{
+//           SpeechRecognition.startListening()
+//         }).catch((error)=>{
+//           console.log(error)
+//         })
+//   }else{
+//     return SpeechRecognition.startListening
+//   }
+
+//   // return () => {
+//   //   second
+//   // }
+// }, [finalTranscript])
+
+
+
+//   return (
+//     <div>
 //       <p>Microphone: {listening ? 'on' : 'off'}</p>
 //       <button onClick={SpeechRecognition.startListening}>Start</button>
+//       <button onClick={SpeechRecognition.stopListening}>Stop</button>
+//       <button onClick={resetTranscript}>Reset</button>
 //       <p>{transcript}</p>
+//       <p>final {finalTranscript}</p>
+//     </div>
+//   );
+// };
+// export default UserDashBoard;
 
-//     </>
-//   )
-// }
-
-// export default UserDashBoard
 
 
-// import React from 'react'
-// // import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
-// function UserDashBoard() {
-//   return (
-//     <>
-//     <div>UserDashBoard</div>
-//     </>
-
-//   )
-// }
-
-// export default UserDashBoard
-
-// transcript,
-//     interimTranscript,
-//     finalTranscript,
-//     resetTranscript,
-//     listening,
-//     browserSupportsSpeechRecognition,
-//     isMicrophoneAvailable
-
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
 import api from '../utils/api';
 
 const UserDashBoard = () => {
@@ -61,53 +82,49 @@ const UserDashBoard = () => {
     listening,
     finalTranscript,
     resetTranscript,
-    browserSupportsSpeechRecognition
+    browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
+  const [botResponse, setBotResponse] = useState('');
+  const [trigger, setTrigger] = useState(false);
 
-const [botResponse, setBotResponse] = useState('')
-const [trigger, setTrigger] = useState(false)
+  useEffect(() => {
+    if (!browserSupportsSpeechRecognition) {
+      return <span>Browser doesn't support speech recognition.</span>;
+    }
 
+    if (finalTranscript === '' || finalTranscript == null || finalTranscript == undefined) {
+      SpeechRecognition.startListening();
+    }
+  }, [finalTranscript, browserSupportsSpeechRecognition]);
 
-useEffect(() => {
-  console.log(listening)
- 
-if(finalTranscript =="" || finalTranscript == null || finalTranscript == undefined){
-  console.log("==>",listening)
-  SpeechRecognition.startListening()
-}
-
-}, [listening])
-
-
-useEffect(() => {
-
-  if(finalTranscript){
-    //axios
-    console.log(finalTranscript)
-    api.post(`/chat/test`,{"prompt":finalTranscript})
+  useEffect(() => {
+    if (finalTranscript) {
+      api
+        .post(`/chat/test`, { prompt: finalTranscript })
         .then((res) => {
-          console.log(res.data.bot)
-          resetTranscript()
-         
-        }).then(()=>{
-          SpeechRecognition.startListening()
-        }).catch((error)=>{
-          console.log(error)
+          console.log(res.data.bot);
+          resetTranscript();
+          setBotResponse(res.data.bot);
         })
-  }else{
-    return SpeechRecognition.startListening
-  }
+        .then(() => {
+          SpeechRecognition.startListening();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      SpeechRecognition.startListening();
+    }
+  }, [finalTranscript, resetTranscript]);
 
-  // return () => {
-  //   second
-  // }
-}, [finalTranscript])
-
-
+  useEffect(() => {
+    if (botResponse) {
+      const synth = window.speechSynthesis;
+      const utterance = new SpeechSynthesisUtterance(botResponse);
+      synth.speak(utterance);
+    }
+  }, [botResponse]);
 
   return (
     <div>
@@ -116,8 +133,10 @@ useEffect(() => {
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
       <p>{transcript}</p>
-      <p>final {finalTranscript}</p>
+      <p>Final: {finalTranscript}</p>
+      <p>Bot Response: {botResponse}</p>
     </div>
   );
 };
+
 export default UserDashBoard;
