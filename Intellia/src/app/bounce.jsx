d12ccd7af05b4059a5d55b17947affbd1fca48e6
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-08 15:23:50 +0000

    auth redux

--- Intellia/src/app/bounce.jsx
+++ Intellia/src/app/bounce.jsx
@@ -5,7 +5,7 @@ import { thisUser } from '../redux/features/auth/loginSlice';
 
 function BounceRoute() {
   const authenticated = useSelector(thisUser);
-  return authenticated.user ? <Navigate to="/" /> : <Outlet />;
+  return authenticated?.user ? <Navigate to="/" /> : <Outlet />;
 }
 
 export default BounceRoute;
